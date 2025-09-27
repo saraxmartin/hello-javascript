a689c5f00c3e2849b7189f2a13961a4d960da4c5
Sara Martin <sara.martin@smadex.com [filter lfs]>
2025-09-27 13:07:09 +0200

    exercises 03,05

--- Basic/03-beginner-exercises.js
+++ Basic/03-beginner-exercises.js
@@ -5,20 +5,68 @@ Vídeo: https://youtu.be/1glVfFxj8a4?t=4733
 
 // 1. Escribe un comentario en una línea
 
+// Esto es un comentario de una línea
+
 // 2. Escribe un comentario en varias líneas
 
+/* Esto es 
+un comentario
+en varias lineas
+*/
+
 // 3. Declara variables con valores asociados a todos los datos de tipo primitivos
+let myInteger = 3
+let myFloat = 2.3
+let myString = "Hi String"
+let myBool = true
+let myNull = null
+let myUndefined = undefined
+let mySymbol = Symbol("mySymbol")
+let myBigInt = BigInt("92999389288888888888888888092830928039238")
 
 // 4. Imprime por consola el valor de todas las variables
+console.log(myInteger)
+console.log(myFloat)
+console.log(myString)
+console.log(myBool)
+console.log(myNull)
+console.log(myUndefined)
+console.log(mySymbol)
+console.log(myBigInt)
 
 // 5. Imprime por consola el tipo de todas las variables
+console.log(typeof myInteger)
+console.log(typeof myFloat)
+console.log(typeof myString)
+console.log(typeof myBool)
+console.log(typeof myNull)
+console.log(typeof myUndefined)
+console.log(typeof mySymbol)
+console.log(typeof myBigInt)
 
 // 6. A continuación, modifica los valores de las variables por otros del mismo tipo
+myInteger = 4
+myFloat = 4.5
+myString = "Different String"
+myBool = false
 
 // 7. A continuación, modifica los valores de las variables por otros de distinto tipo
+myInteger = 4.5
+myFloat = "Hola"
+myString = 7.8
+myBool = 7
 
 // 8. Declara constantes con valores asociados a todos los tipos de datos primitivos
+const otroInt = 8
+const otroFloat = 8.9
+const otroString = "Buenas"
+const otroBool = true
 
 // 9. A continuación, modifica los valores de las constantes
+//otroInt = 9
+//otroFloat = 7.8
+//otroString = "Adios"
+//otroBool = false
+
 
 // 10. Comenta las líneas que produzcan algún tipo de error al ejecutarse
--- Basic/05-operators-exercises.js
+++ Basic/05-operators-exercises.js
@@ -4,22 +4,55 @@ Vídeo: https://youtu.be/1glVfFxj8a4?t=6458
 */
 
 // 1. Crea una variable para cada operación aritmética
+let a = 3
+let b = 4
+
+let mySum = a + b
+let myResta = a - b
+let myMultiplication = a * b
+let myDivision = a / b
+let myExponential = a ** b
+let myModule = a % b
+let myIncrement = a++
+let myDecrement = a--
 
 // 2. Crea una variable para cada tipo de operación de asignación,
 //    que haga uso de las variables utilizadas para las operaciones aritméticas
+let c = a+=2
+let d = a-=2
+let e = a*=2
+let f = a/=2
+let g = a%=2
+let h = a**=2
 
 // 3. Imprime 5 comparaciones verdaderas con diferentes operadores de comparación
+console.log(a == a)
+console.log(typeof a == typeof e)
+console.log(a != f)
+console.log(e > b)
 
 // 4. Imprime 5 comparaciones falsas con diferentes operadores de comparación
+console.log(b == a)
+console.log(g >= b)
+console.log(a === b) // same type but not same value
+console.log(a != a)
 
 // 5. Utiliza el operador lógico and
+console.log(a, b)
+console.log(a<5 && b==4)
 
 // 6. Utiliza el operador lógico or
+console.log(a==5 || b==4)
 
 // 7. Combina ambos operadores lógicos
+console.log(a==1 && b==4 || b<4)
 
 // 8. Añade alguna negación
+console.log(a!=2 && b!=3)
 
 // 9. Utiliza el operador ternario
+const isCloudy = true
+isCloudy ? console.log("It's cloudy") : console.log("It's not cloudy")
 
 // 10. Combina operadores aritméticos, de comparáción y lógicas
+console.log()
